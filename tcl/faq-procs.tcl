8324f6ea7e7613b29bb3378f75e75bd917f2a60c
Luis de la Fuente <lfuente@it.uc3m.es>
2005-11-16 11:34:52 +0000

    new_faq procedure created

--- tcl/faq-procs.tcl
+++ tcl/faq-procs.tcl
@@ -7,6 +7,51 @@ ad_library {
     @Modifyed by Gerardo Morales <gmorales@galileo.edu>
 }
 
+namespace eval faq {
+    ad_proc -public get_package_id {
+        -community_id
+    } {
+        if {[info exist community_id]} { } else { set community_id [ad_conn community_id] }
+
+        db_1row get_faqs_package {}
+        return $package_id
+        
+    }
+
+
+    ad_proc -public faq_new {
+        -package_id
+        {-separate_p "f"}
+        -faq_name:required
+    } {
+        if {[info exist package_id]} { } else { set package_id [ad_conn package_id] }
+        set faq_id [db_nextval acs_object_id_seq]
+        set user_id [ad_conn user_id]
+        set creation_ip [ad_conn host]
+
+         db_transaction {
+            db_exec_plsql create_faq {
+            begin
+              :1 := faq.new_faq (
+                    faq_id => :faq_id,
+                        faq_name => :faq_name,
+                    separate_p => :separate_p,
+                    creation_user => :user_id,
+                            creation_ip => :creation_ip,
+                        context_id => :package_id
+                    );
+            end;
+            }
+        }
+
+    return $faq_id
+        
+    }
+
+
+}
+
+
 
 namespace eval faq::notification {
 
@@ -24,3 +69,5 @@ namespace eval faq::notification {
 
     }
 }
+
+
