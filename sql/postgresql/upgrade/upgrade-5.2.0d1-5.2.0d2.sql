c871321c313dd7d840560093b2a88e29a31b0a35
Don Baccus <dhogaza@pacifier.com>
2006-08-08 21:26:46 +0000

    First shot at merging oacs-5-2 apps used in .LRN to HEAD.

--- sql/postgresql/upgrade/upgrade-5.2.0d1-5.2.0d2.sql
+++ sql/postgresql/upgrade/upgrade-5.2.0d1-5.2.0d2.sql
@@ -1,5 +1,5 @@
 -- fix name method, update package
 
-update acs_objects set name_method = 'faq__name' where name_method = 'faq.name' and object_type = 'faq';
+update acs_object_types set name_method = 'faq__name' where name_method = 'faq.name' and object_type = 'faq';
 
 \i ../faq-package-create.sql
