0c749199db3e4dc8d3f2048090bb59363351cbc7
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2016-05-15 20:15:22 +0000

    - provide support for old installations, with other constaint names

--- sql/postgresql/upgrade/upgrade-5.9.0-5.9.1d1.sql
+++ sql/postgresql/upgrade/upgrade-5.9.0-5.9.1d1.sql
@@ -4,7 +4,9 @@
 
 ALTER TABLE faqs
       DROP constraint IF EXISTS faqs_disabled_p_check,
-      DROP constraint IF EXISTS faqs_separate_p_check,      
+      DROP constraint IF EXISTS faqs_separate_p_check,
+      DROP constraint IF EXISTS faqs_disabled_p_ck,
+      DROP constraint IF EXISTS faqs_separate_p_ck,      
       ALTER COLUMN disabled_p DROP DEFAULT,
       ALTER COLUMN disabled_p TYPE boolean
       USING disabled_p::boolean,
