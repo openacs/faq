ed3802c80cd6bf789a558fb7992b63a980b1773a
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2017-04-21 20:06:35 +0000

    Fix spelling errors

--- tcl/apm-callback-procs.tcl
+++ tcl/apm-callback-procs.tcl
@@ -9,7 +9,7 @@ namespace eval faq::apm_callback {}
 
 ad_proc -private faq::apm_callback::package_install { 
 } {
-    Does the integration whith the notifications package. 
+    Does the integration with the notifications package. 
 } {
     db_transaction {
 
@@ -35,7 +35,7 @@ ad_proc -private faq::apm_callback::package_install {
 
 ad_proc -private faq::apm_callback::package_uninstall {
 } {
-    Remove the integration whith the notification package
+    Remove the integration with the notification package
 } {
 
     db_transaction {
