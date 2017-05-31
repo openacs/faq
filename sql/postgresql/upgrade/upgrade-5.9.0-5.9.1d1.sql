d5ae6e23aef436944e915ce0e033a82aa3dd0cc1
michaela <>
2017-05-31 12:47:26 +0000

    Add another constraint name variant as it existed in our production system

--- sql/postgresql/upgrade/upgrade-5.9.0-5.9.1d1.sql
+++ sql/postgresql/upgrade/upgrade-5.9.0-5.9.1d1.sql
@@ -6,7 +6,9 @@ ALTER TABLE faqs
       DROP constraint IF EXISTS faqs_disabled_p_check,
       DROP constraint IF EXISTS faqs_separate_p_check,
       DROP constraint IF EXISTS faqs_disabled_p_ck,
-      DROP constraint IF EXISTS faqs_separate_p_ck,      
+      DROP constraint IF EXISTS faqs_separate_p_ck,
+      DROP constraint IF EXISTS faqs_disabled_p,
+      DROP constraint IF EXISTS faqs_separate_p,
       ALTER COLUMN disabled_p DROP DEFAULT,
       ALTER COLUMN disabled_p TYPE boolean
       USING disabled_p::boolean,
