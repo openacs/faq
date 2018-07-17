e5909c69abe41d2ea1185d830fb45f85909d6e7b
Hector Romojaro <hector.romojaro@gmail.com>
2018-07-17 13:32:18 +0000

    Whitespace changes + add editor hints

--- lib/faq-add-edit.tcl
+++ lib/faq-add-edit.tcl
@@ -9,14 +9,14 @@ ad_form -name faq_add_edit -mode $mode -action "faq-add-edit" -form {
     {separate_p:text(select) {label "#faq.QA_on_Separate_Pages#"} { options {{[_ faq.No] f} {[_ faq.Yes] t}} } }
 } -select_query_name get_faq -new_data {
     set user_id [ad_conn user_id]
-    set creation_ip [ad_conn host] 
-    set package_id [ad_conn package_id] 
+    set creation_ip [ad_conn host]
+    set package_id [ad_conn package_id]
     set faq_id [db_exec_plsql create_faq {}]
 } -edit_data {
     db_dml edit_faq {}
 } -after_submit {
    if { ![info exists return_url] || $return_url eq "" } {
-       set return_url [export_vars -base one-faq { faq_id }] 
+       set return_url [export_vars -base one-faq { faq_id }]
    }
    ad_returnredirect $return_url
    ad_script_abort
