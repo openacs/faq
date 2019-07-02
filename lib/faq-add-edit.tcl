3125d5fbaa089fd460eb49621967bce06b462958
Antonio Pisano <antonio@elettrotecnica.it>
2019-07-02 12:40:40 +0000

    Add ad_include_contract

--- lib/faq-add-edit.tcl
+++ lib/faq-add-edit.tcl
@@ -1,5 +1,11 @@
+ad_include_contract {
+    FAQ add/edit UI
+} {
+    {mode ""}
+    {return_url ""}
+}
 
-if { ![info exists mode] || $mode eq "" } {
+if { $mode eq "" } {
     set mode "edit"
 }
 
@@ -15,7 +21,7 @@ ad_form -name faq_add_edit -mode $mode -action "faq-add-edit" -form {
 } -edit_data {
     db_dml edit_faq {}
 } -after_submit {
-   if { ![info exists return_url] || $return_url eq "" } {
+   if { $return_url eq "" } {
        set return_url [export_vars -base one-faq { faq_id }]
    }
    ad_returnredirect $return_url
